e64ccd8fa673caff96f179fae2a7f82c53c63255
Zhaoyilunnn <dilved1014@gmail.com>
2023-05-19 13:47:43 +0000

    qdao:: init setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='qdao',
+    version='0.1',
+    packages=['qdao'],
+    install_requires=[
+
+    ],
+    entry_points={
+        'console_scripts': [
+
+        ],
+    },
+)
+
