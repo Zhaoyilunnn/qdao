5881b9155d728d0fe3791e4a205eda56f02158ed
Zhaoyilunnn <dilved1014@gmail.com>
2023-11-23 02:56:54 +0000

    chore: add an example.py

---
+++ example.py
@@ -0,0 +1,31 @@
+from qdao import Engine
+from qiskit.circuit.random import random_circuit
+from qiskit import transpile
+from qiskit_aer import Aer
+
+num_qubits = 12
+num_primary = 10
+num_local = 8
+
+# Create a qiskit quantum circuit `circ`
+circ = random_circuit(num_qubits, 10, measure=False, max_operands=2)
+backend = Aer.get_backend("aer_simulator")
+circ = transpile(circ, backend=backend)
+
+# `num_primary`: size of a compute unit
+# `num_local`: size of a storage unit
+eng = Engine(circuit=circ, num_primary=num_primary, num_local=num_local)
+eng.run()
+
+# First transform qiskit circuit to a quafu circuit
+from quafu import QuantumCircuit
+quafu_circ = QuantumCircuit(1)
+quafu_circ.from_openqasm(circ.qasm())
+
+# Create a new engine using quafu backend
+eng = Engine(circuit=quafu_circ, num_primary=num_primary, num_local=num_local, backend="quafu")
+eng.run()
+
+from qdao.util import retrieve_sv
+res = retrieve_sv(num_qubits, num_local=num_local)
+print(res)
